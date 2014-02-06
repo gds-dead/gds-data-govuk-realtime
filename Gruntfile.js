1d319e4156cf569c03bcc1ee5dc1d702cddc33ad
Mat Johnson <mat@demotive.com>
2014-02-06 17:20:54 +0000

    Tweaking grunt flow

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,37 +2,47 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+
+        // CSS
         sass: {
-            dist : {
+            dev : {
                 options: {
                     style: 'expanded'
                 },
                 files: {
                     'public/css/main.css': 'assets/scss/main.scss'
                 }
+            },
+            dist : {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'public/css/main.css': 'assets/scss/main.scss'
+                }
             } 
         },
-        
+
+        // JS
         concat: {
             dist: {
-                src: [
-                    'assets/js/vendor/jquery-2.0.3.js',
-                    'assets/js/vendor/raphael.js',
-                    'assets/js/vendor/g.raphael-min.js',
-                    'assets/js/vendor/g.pie.js',
-                    'assets/js/items/*.js',
-                    'assets/js/main.js'
-                ],
-                dest: 'public/js/app.js',
-            }
-        },
-    
-        uglify: {
-            build: {
-                src: 'public/js/app.js',
+                // the files to concatenate
+                src: ['assets/js/**/*.js'],
+                // the location of the resulting JS file
                 dest: 'public/js/app.js'
             }
         },
+        uglify: {
+            dist: {
+                files: {
+                    '<%= concat.dist.dest %>': ['<%= concat.dist.dest %>']
+                }
+            }
+        },
+
+        // Cache bust
+        // For build - empty the folders in /public/
+        clean: ["public/css/*", "public/js/*"],
 
         hashres: {
           // Global options
@@ -66,14 +76,14 @@ module.exports = function(grunt) {
         watch: {
             scripts: {
                 files: ['assets/js/*.js', 'assets/js/items/*.js'],
-                tasks: ['concat', 'uglify', 'hashres'],
+                tasks: ['concat', 'hashres'],
                 options: {
                     spawn: false,
                 },
             },
             css: {
                 files: ['assets/scss/*.scss'],
-                tasks: ['sass', 'hashres'],
+                tasks: ['sass:dev', 'hashres'],
                 options: {
                     spawn: false,
                 }
@@ -85,9 +95,13 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-hashres');
     grunt.loadNpmTasks('grunt-contrib-watch');
     
     grunt.registerTask('default', ['watch']);
+    
+    grunt.registerTask('test', ['sass:dev', 'concat', 'hashres']);
+    grunt.registerTask('build', ['clean', 'sass:dist', 'concat', 'uglify', 'hashres']);
 
 };
